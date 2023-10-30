7e4334e19d0cfa8220abd3fe6dca891e870b03b6
deiju4033 <sdepin4@gmail.com>
2023-10-30 17:17:08 +0530

    Ecommerce website

---
+++ java.js
@@ -0,0 +1,14 @@
+
+
+
+let menu =document.querySelector(`#menu-icon`);
+let navmenu =document.querySelector(`.navmenu`);
+
+
+menu.onclick =() => {
+
+menu.classList.toggle(`bx-x`);
+navmenu.classList.toggle(`open`);
+
+
+}
